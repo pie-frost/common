2b4379667a527a97e53900dcf09d9e1d95dcbb83
Paragon Initiative Enterprises <security@paragonie.com>
2023-07-30 03:07:58 -0400

    Fallback if superglobals are not set

--- src/Router.php
+++ src/Router.php
@@ -76,8 +76,8 @@ class Router
      */
     protected function routeSuperGlobal(): HandlerInterface
     {
-        $httpMethod = $_SERVER['REQUEST_METHOD'];
-        $uri = $_SERVER['REQUEST_URI'];
+        $httpMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $uri = $_SERVER['REQUEST_URI'] ?? '';
         // Strip query string (?foo=bar) and decode URI
         if (false !== $pos = strpos($uri, '?')) {
             $uri = substr($uri, 0, $pos);
